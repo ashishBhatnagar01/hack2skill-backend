8660b16203799ed6bf671db518b917ea7fb99edc
Ashish Bhatnagar <bhatnagarashish16@gmail.com>
2022-03-26 04:02:35 +0530

    Fixed

--- controllers/Data.js
+++ controllers/Data.js
@@ -4,41 +4,69 @@ const Users = require("../models/Users");
 
 module.exports.getData = async (req, res) => {
   // console.log(req.query)
-  // var pageSize = Number(10)
-  // var pageNumber = Number(req.query.pageNumber)
-  const data = await Users.aggregate([
-    {
-      $lookup: {
-        from: "teams",
-        localField: "email",
-        foreignField: "email",
-        as: "teamData",
+  var pageSize = Number(req.query.pageSize)
+  var pageNumber = Number(req.query.pageNumber)
+  var data;
+  if(req.query.pageNumber){
+     data = await Users.aggregate([
+      {
+        $lookup: {
+          from: "teams",
+          localField: "email",
+          foreignField: "email",
+          as: "teamData",
+        },
       },
-    },
-    {
-      $unwind: "$teamData",
-    },
-    {
-      $project: {
-        full_name: 1,
-        email: 1,
-        number: 1,
-        city: 1,
-        url: 1,
-        "teamData.team_name": 1,
-        _id: 0,
+      {
+        $unwind: "$teamData",
       },
-    },
-    // {
-    //   $skip: pageSize * pageNumber
-    // },
-    // {
-    //     $limit:pageSize
-    // }
-  ]);
-  console.log(data.length);
+      {
+        $project: {
+          full_name: 1,
+          email: 1,
+          number: 1,
+          city: 1,
+          url: 1,
+          "teamData.team_name": 1,
+          _id: 0,
+        },
+      },
+      {
+        $skip: pageSize * pageNumber
+      },
+      {
+          $limit:pageSize
+      }
+    ]);
+  }
+  else{
+     data = await Users.aggregate([
+      {
+        $lookup: {
+          from: "teams",
+          localField: "email",
+          foreignField: "email",
+          as: "teamData",
+        },
+      },
+      {
+        $unwind: "$teamData",
+      },
+      {
+        $project: {
+          full_name: 1,
+          email: 1,
+          number: 1,
+          city: 1,
+          url: 1,
+          "teamData.team_name": 1,
+          _id: 0,
+        },
+      },
+    ]);
+  }
   
   return res.json({
-    message: data,
+    data: data,
   });
 };
